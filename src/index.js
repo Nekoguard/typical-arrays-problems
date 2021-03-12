a3931c3d0f5dad04b798e128e42d5d7481d1f723
Nekoguard <nekoguard@protonmail.com>
2021-03-12 15:06:51 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  } else {
+    let minResult = Math.min.apply(null, array);
+    return minResult;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  } else {
+    let maxResult = Math.max.apply(null, array);
+    return maxResult;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avgResult;
+  let sum = 0;
+  
+  if (!array || array.length == 0) {
+    return 0;
+  } else {
+    for (let elem of array) {
+      sum += elem;
+      avgResult = sum / array.length;
+    }
+    return avgResult;
+  }
 }
